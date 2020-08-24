ca84c153b0df7d738c7ce12e893e575645b8b753
David C Black <david.black@doulos.com>
2020-08-24 04:16:32 -0500

    Refactor slightly. More importantly changed from using system() to execvp() to gain proper efficiency of memory usage

--- examples/backtrace/backtrace.cpp
+++ examples/backtrace/backtrace.cpp
@@ -1,32 +1,65 @@
 #include "backtrace.hpp"
 #include <iostream>
+#include <iomanip>
 #include <fstream>
+#include <vector>
 #include <string>
+#include <unistd.h>
+
+using namespace std::literals;
 
 void backtrace(int argc, const char* argv[])
 {
   if( argc > 1 and std::string(argv[1]) == "-backtrace" ) {
 
-    // Construct arguments into a single character string to be interpreted
-    std::string args;
-    for( int i=2; i<argc; ++i ) args += std::string(argv[i]) + " ";
-    while( not args.empty() and args.back() == ' ' ) args.pop_back();
-    // Escape single quotes
-    size_t pos=0;
-    while(  ( pos = args.find_first_of( '\'', pos ) ) != std::string::npos ) {
-      args.replace( pos, 1, "\\'" );
-      pos += 2;
+    // Construct gdb set args command with remaining arguments
+    std::string run = "run";
+    for( int i=2; i<argc; ++i ) {
+      std::string arg{argv[i]};
+      // Is quoting needed due to internal white-space or single quotes?
+      using Str=std::string;
+      if( arg.find_first_of('"') != Str::npos ) {
+        // Escape all double quotes
+        size_t pos = 0;
+        while( (pos=arg.find_first_of('"',pos)) != Str::npos ) {
+          arg.replace( pos, 1, "\\\"" );
+          pos += 2;
+        }
+      }
+      run += " \""s + arg + "\""s;
     }
+    std::cout << "DEBUG: {" << run << "}" << std::endl;
 
-    // Create gdb command
-    std::string command{ "echo y | gdb -ex 'set args %s' -ex run -ex backtrace -ex quit %s" };
-    std::string program{ argv[0] };
-    // Adjust command to replace %s placeholders
-    command.replace( command.find( "%s" ), 2, args );
-    command.replace( command.find( "%s" ), 2, program );
+    // Create gdb gdb options
+    const char* new_argv[] =
+    { "gdb"
+    , "-batch"
+    , "-ex",   run.c_str()
+    , "-ex",   "backtrace"
+    , "-ex",   "quit"
+    , argv[0]
+    , nullptr
+    };
 
     // Run the simulation under gdb
-    std::cout << "DEBUG: " << command << std::endl;
-    exit( system( command.c_str() ) );
+    int status_code = execvp( "gdb", reinterpret_cast<char*const*>(&new_argv) );
+    if (status_code == -1) {
+      std::cout
+        << "exec failed"
+        << " on line " << __LINE__ << " of " << __FILE__
+        << "\n"
+        ;
+      exit(1);
+    }
+    std::cout << "This line will not be printed if execvp() runs correctly\n";
+    exit(1);
   }
+  std::cout << "Running " << argv[0];
+  if ( argc == 1 ) std::cout << " with no arguments.\n";
+  else             std::cout << " with arguments:\n";
+  for( int i=1; i<argc; ++i ) {
+    std::string arg(argv[i]);
+    std::cout << "  " << std::setw(2) << i << ": {" << arg << "}" << std::endl;
+  }
+  std::cout << std::string(80,'-') << std::endl;
 }
